9a48dc43ac9857dce635b02562f4e8b66da247b5
Janak <doon.29292@gmail.com>
2020-09-13 17:14:45 +0530

    Update numpy

--- numpy_pandas/numpy_notes.py
+++ numpy_pandas/numpy_notes.py
@@ -1,4 +1,4 @@
-from numpy import array, ones, zeros, linspace, eye, random, sqrt, exp, sin, log, sum, std
+from numpy import array, ones, zeros, linspace, eye, random, sqrt, exp, sin, log, sum, std, arange
 
 x = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
  [[10, 11, 12], [13, 14, 15], [16, 17, 18]],
@@ -6,7 +6,22 @@ x = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
 
 arr = array(x)
 
+
+print(arr.max())
+print(arr.argmax())
+
+
+print(arange(0, 20, 3))
+print(linspace(0, 1, 10))
+print(random.rand(5))
+print(random.rand(5, 5))
+print(random.randn(5))
+print(eye(4))
+print(zeros(5))
+print(ones((5,5)))
+
 print(arr.shape)
+print(arr.reshape(27))
 
 print(arr[0])
 
@@ -21,3 +36,17 @@ print(arr[1: , 2:])
 print(arr[:, :, :1])
 
 print(std(arr))
+print(std(arr, axis=0))
+print(std(arr, axis=1))
+print(std(arr, axis=2))
+
+
+print(arr)
+print(arr % 2 ==0)
+print(arr[arr % 2 ==0])
+print(len(arr[arr % 2 ==0]))
+
+
+print(arr - 10)
+print((arr - 10)/10)
+print((arr - 10)/0)
